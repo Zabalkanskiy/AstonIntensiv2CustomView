74422f85c454eae8c5bcdb0f3dee3d56cf9fbc3c
Zabalkanskiy <azabalkanskij@hotmail.com>
2023-11-30 17:01:10 +0300

    add value in repository and write method in model for change

--- app/src/main/java/com/seven/astonintensiv2customview/model/CustomViewRepository.kt
+++ app/src/main/java/com/seven/astonintensiv2customview/model/CustomViewRepository.kt
@@ -4,4 +4,8 @@ import androidx.lifecycle.MutableLiveData
 
 class CustomViewRepository {
     val angle = MutableLiveData<Float>()
+    var currentAngle = 0f
+    var finalAngle = 0f
+
+    val urlPlaceHolder = "https://loremflickr.com/640/360"
 }
